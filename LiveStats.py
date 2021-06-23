fb08a8684704946ded17e4568533095f07a16c85
carlo <carlo.vanderstoep@gmail.com>
2021-06-23 16:59:45 +0200

    first commit

--- LiveStats.py
+++ LiveStats.py
@@ -6,12 +6,11 @@ utility program that just changes the config to output anything to console.
 """
 import InverterExport
 
-
 if __name__ == "__main__":
     inverter_exporter = InverterExport.InverterExport('config.cfg')
 
     inverter_exporter.override_config('general', 'enabled_plugins',
-                                   'ConsoleOutput')
+                                      'ConsoleOutput')
     inverter_exporter.override_config('log', 'type', 'console')
     inverter_exporter.override_config('log', 'level', 'debug')
     inverter_exporter.run()
